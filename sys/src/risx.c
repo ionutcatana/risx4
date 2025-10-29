1195e318607d8f55a388d9210736c890ae1bad4c
Ioan-Alexandru Catana <ionutcatana@member.fsf.org>
2025-10-29 18:29:18 +0200

    turn screen white

--- sys/src/risx.c
+++ sys/src/risx.c
@@ -1,4 +1,5 @@
 #include <stdbool.h>
+#include <stddef.h>
 #include <stdint.h>
 #include <stdnoreturn.h>
 #include "multiboot2.h"
@@ -16,7 +17,7 @@ uint32_t validate_mbi(uint32_t magic, uintptr_t addr) {
     #define ALIGN_UP(value, align) (((value) + (align) - 1) & ~((align) - 1))
 
     uintptr_t tag_addr = addr + 8;
-    struct multiboot2_tag *tag = (struct multiboot2_tag *)tag_addr;
+    struct multiboot2_tag* tag = (struct multiboot2_tag*)tag_addr;
 
     while (tag->type != MULTIBOOT2_TAG_TYPE_END) {
         switch (tag->type) {
@@ -39,11 +40,35 @@ uint32_t validate_mbi(uint32_t magic, uintptr_t addr) {
                 break;
 
             case MULTIBOOT2_TAG_TYPE_FRAMEBUFFER:
+                struct multiboot2_tag_framebuffer* fb_tag = (struct multiboot2_tag_framebuffer*)tag;
+                uint8_t* framebuffer = (uint8_t*)(uintptr_t)fb_tag->common.framebuffer_addr;
+
+                for (size_t i = 0; i < fb_tag->common.framebuffer_height; i++) {
+                    for (size_t j = 0; j < fb_tag->common.framebuffer_width; j++) {
+
+                        size_t bytes_per_pixel = fb_tag->common.framebuffer_bpp / 8;
+                        size_t pixel_offset = i * fb_tag->common.framebuffer_pitch + j * bytes_per_pixel;
+
+                        for (size_t k = 0; k < bytes_per_pixel; k++) {
+                            framebuffer[pixel_offset + k] = 0xFF;
+                        }
+                    }
+                }
+
+                // if (fb_tag->common.framebuffer_type == MULTIBOOT2_FRAMEBUFFER_TYPE_RGB) {
+                //     //
+                // } else if (fb_tag->common.framebuffer_type == MULTIBOOT2_FRAMEBUFFER_TYPE_INDEXED)  {
+                //     //
+                // } else {
+                //     // I have no idea what EGA text mode is
+                //     return 1;
+                // }
+
                 break;
         }
 
         tag_addr += ALIGN_UP(tag->size, 8);
-        tag = (struct multiboot2_tag *)tag_addr;
+        tag = (struct multiboot2_tag*)tag_addr;
     }
 
     #undef ALIGN_UP
