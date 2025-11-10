042e0423a1211e446538a12b46cbbdd8c8024262
Ioan-Alexandru Catana <ionutcatana@member.fsf.org>
2025-11-10 21:14:49 +0200

    add panic+main rust-like structure

---
+++ sys/include/arch/x86/halt.h
@@ -0,0 +1,7 @@
+#ifndef X86_HALT_H
+#define X86_HALT_H 1
+
+// arch/x86/halt.S
+extern void halt_forever(void);
+
+#endif
--- sys/src/main.c
+++ sys/src/main.c
@@ -4,10 +4,18 @@
 #include <stddef.h>
 #include <stdnoreturn.h>
 
-noreturn void risx(void) {
-    kprintf("Entered RISX. Setup successful.\n");
+noreturn void halt(void);
+noreturn void panic(const char* message) {
+    kprintf("PANIC: %s\n", message);
     kfflush();
-    while (true);
+    halt();
+}
+
+noreturn void risx(void) {
+    kprintf("Entered RISX\nSetup successful\n");
+    kfflush();
+
+    panic("Unexpected return from scheduler");
 }
 
 
