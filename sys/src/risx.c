32b29ca2f2ade7f8c5aed82f8e0e42166c39342d
Ioan-Alexandru Catana <ionutcatana@member.fsf.org>
2025-11-18 14:27:47 +0200

    remove limine.h

--- sys/src/risx.c
+++ sys/src/risx.c
@@ -8,15 +8,12 @@
 
 noreturn void panic(const char* message) {
     kprintf("PANIC: %s\n", message);
-    kfflush();
     abort();
 }
 
 noreturn void risx(void) {
     setup();
-
     kprintf("Entered RISX\nSetup successful\n");
-    kfflush();
 
     panic("Unexpected return from scheduler");
 }
