09b43574a3cd8627146036f0bd0b8c3ccd9d36c5
Ioan-Alexandru Catana <ionutcatana@member.fsf.org>
2025-11-19 20:22:48 +0200

    redo string.c

--- sys/src/libk/string.c
+++ sys/src/libk/string.c
@@ -4,102 +4,63 @@
 #include <stddef.h>
 #include <stdint.h>
 
-int memcmp(const void* aptr, const void* bptr, size_t n) {
-	const uint8_t* a = (const uint8_t*)aptr;
-	const uint8_t* b = (const uint8_t*)bptr;
-	for (size_t i = 0; i < n; i++) {
-		if (a[i] < b[i]) {
-			return -1;
-		} else if (b[i] < a[i]) {
-			return 1;
-		}
-	}
-
-	return 0;
-}
-
 void* memcpy(void* restrict dstptr, const void* restrict srcptr, size_t n) {
-	uint8_t* dst = (uint8_t*)dstptr;
-	const uint8_t* src = (const uint8_t*)srcptr;
-
-    while (n >= 8) {
-        *(uint64_t*)dst = *(const uint64_t*)src;
-        dst += 8;
-        src += 8;
-        n -= 8;
+    unsigned char* dst = (unsigned char*)dstptr;
+    const unsigned char* src = (const unsigned char*)srcptr;
+    for (size_t i = 0; i < n; i++) {
+        dst[i] = src[i];
     }
-
-	for (size_t i = 0; i < n; i++) {
-		dst[i] = src[i];
-	}
-
     return dstptr;
 }
 
 void* memmove(void* dstptr, const void* srcptr, size_t n) {
-	uint8_t* dst = (uint8_t*)dstptr;
-	const uint8_t* src = (const uint8_t*)srcptr;
-	if (dst < src) {
-		for (size_t i = 0; i < n; i++) {
-			dst[i] = src[i];
-        }
-	} else {
-		for (size_t i = n; i != 0; i--) {
-			dst[i-1] = src[i-1];
-        }
-	}
+    unsigned char* dst = (unsigned char*)dstptr;
+    const unsigned char* src = (const unsigned char*)srcptr;
 
+    if (dst < src) {
+        for (size_t i = 0; i < n; i++)
+            dst[i] = src[i];
+    } else {
+        for (size_t i = n; i != 0; i--)
+            dst[i-1] = src[i-1];
+    }
     return dstptr;
 }
 
-void* memset(void* bufptr, int value, size_t n) {
-	uint8_t* buf = (uint8_t*)bufptr;
-	for (size_t i = 0; i < n; i++) {
-		buf[i] = (uint8_t) value;
+int memcmp(const void* aptr, const void* bptr, size_t n) {
+    const unsigned char* a = (const unsigned char*)aptr;
+    const unsigned char* b = (const unsigned char*)bptr;
+    for (size_t i = 0; i < n; i++) {
+        if (a[i] < b[i]) return -1;
+        if (b[i] < a[i]) return 1;
     }
+    return 0;
+}
 
-	return bufptr;
+void* memset(void* bufptr, int value, size_t n) {
+    unsigned char* buf = (unsigned char*)bufptr;
+    for (size_t i = 0; i < n; i++) {
+        buf[i] = (unsigned char)value;
+    }
+    return bufptr;
+}
+
+char* strcpy(char* restrict dst, const char* restrict src) {
+    char* ret = dst;
+    while ((*dst++ = *src++));
+    return ret;
 }
 
 int strcmp(const char* s1, const char* s2) {
-	size_t i = 0;
-	while (true) {
-		unsigned char a_byte = s1[i];
-		unsigned char b_byte = s2[i];
-
-        if(!a_byte && !b_byte) {
-			return 0;
-		}
-
-		// If only one char is null, one of the following cases applies.
-		if(a_byte < b_byte) {
-			return -1;
-        }
-		if(a_byte > b_byte) {
-			return 1;
-        }
-
-		i++;
-	}
-}
-
-char* strcpy(char* restrict dst, const char* src) {
-	char *dst_bytes = (char *)dst;
-	char *src_bytes = (char *)src;
-	while (*src_bytes) {
-		*(dst_bytes++) = *(src_bytes++);
+    while (*s1 && (*s1 == *s2)) {
+        s1++;
+        s2++;
     }
-
-	*dst_bytes = 0;
-
-    return dst;
+    return *(const unsigned char*)s1 - *(const unsigned char*)s2;
 }
 
 size_t strlen(const char* str) {
-	size_t len = 0;
-	while (str[len]) {
-		len++;
-    }
-
-	return len;
+    size_t len = 0;
+    while (str[len]) len++;
+    return len;
 }
