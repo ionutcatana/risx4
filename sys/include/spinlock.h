9f1e4960128a8967605fd6d1bc5d997f581b5cd2
Ioan-Alexandru Catana <ionutcatana@member.fsf.org>
2025-11-24 14:23:51 +0200

    continue work on spinlocks

--- sys/include/spinlock.h
+++ sys/include/spinlock.h
@@ -1,10 +1,14 @@
 #ifndef SPINLOCK_H
 #define SPINLOCK_H 1
 
+#include <stdint.h>
 #include <stdbool.h>
 
+#define LOCKHELD 1
+#define LOCKFREE 0
+
 struct spinlock {
-    bool locked;
+    uint64_t locked; // not `bool` because i want it to be register width
 
 #if defined(RISXDEBUG)
     const char* name;
--- sys/src/spinlock.c
+++ sys/src/spinlock.c
@@ -1,6 +1,9 @@
+#include "arch/x86/atomic.h"
 #include <spinlock.h>
 
 #include <stdbool.h>
+
+// TODO: interrupts safety: pushinterrupts, popinterrupts
 
 void initlock(struct spinlock* lock, const char* name) {
     lock->locked = false;
@@ -10,14 +13,15 @@ void initlock(struct spinlock* lock, const char* name) {
 }
 
 void acquirelock(struct spinlock* lock) {
-    return;
+    while(exchange(&lock->locked, LOCKHELD) != LOCKFREE);
 }
 
 void releaselock(struct spinlock* lock) {
-    return;
+    exchange(&lock->locked, LOCKFREE);
 }
 
 bool holdinglock(struct spinlock* lock) {
-    return true;
+    bool result = lock->locked;
+    return result;
 }
 
