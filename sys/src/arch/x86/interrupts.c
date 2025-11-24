17d63df74565a731d71688759c1d93cada7fa81a
Ioan-Alexandru Catana <ionutcatana@member.fsf.org>
2025-11-24 17:07:34 +0200

    more work on push/pop int (still wip)

--- sys/src/arch/x86/interrupts.c
+++ sys/src/arch/x86/interrupts.c
@@ -1,21 +1,33 @@
 #include <arch/x86/interrupts.h>
 #include <arch/x86/registers.h>
+#include <risx.h>
 
 #include <stdint.h>
 
-int64_t ninterrupts = 0;
-
+static int64_t ninterrupts = 0;
+static uint64_t intenabled = 1;
 
 void pushinterrupts() {
-//  uint64_t rflags = readrflags();
+    uint64_t rflags = readrflags();
     disableinterrupts();
+
     ninterrupts++;
+    if (ninterrupts == 0) {
+        intenabled = rflags & RFLAGS_IF;
+    }
 }
 
 void popinterrupts() {
     ninterrupts--;
+    if (ninterrupts < 0) {
+        panic("decremented ninterrupts past 0.");
+    }
+
+    if (readrflags() & RFLAGS_IF) {
+        panic("interrupts enabled flag is set.");
+    }
+
     if (ninterrupts == 0) {
         enableinterrupts();
     }
-    return;
 }
