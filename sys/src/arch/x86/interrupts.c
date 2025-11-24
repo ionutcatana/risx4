ed543acc7db73a55aaa7709b5d45c969ebf35569
Ioan-Alexandru Catana <ionutcatana@member.fsf.org>
2025-11-24 14:55:57 +0200

    easy stuff

--- sys/src/arch/x86/interrupts.c
+++ sys/src/arch/x86/interrupts.c
@@ -1,13 +1,21 @@
 #include <arch/x86/interrupts.h>
+#include <arch/x86/registers.h>
 
 #include <stdint.h>
 
 int64_t ninterrupts = 0;
 
+
 void pushinterrupts() {
-    return;
+//  uint64_t rflags = readrflags();
+    disableinterrupts();
+    ninterrupts++;
 }
 
 void popinterrupts() {
+    ninterrupts--;
+    if (ninterrupts == 0) {
+        enableinterrupts();
+    }
     return;
 }
