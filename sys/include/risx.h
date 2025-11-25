8ef4735fbde3e02c84351942772fe03cc0a2d9e8
Ioan-Alexandru Catana <ionutcatana@member.fsf.org>
2025-11-25 14:39:40 +0200

    add a header file for configuration

--- sys/include/risx.h
+++ sys/include/risx.h
@@ -1,6 +1,7 @@
 #ifndef RISX_H
 #define RISX_H 1
 
+#include <risx/config.h>
 #include <stdnoreturn.h>
 
 noreturn void panic(const char* message);
---
+++ sys/include/risx/config.h
@@ -0,0 +1,11 @@
+#ifndef CONFIG_H
+#define CONFIG_H
+
+/* RISX release number and codename                                           */
+#define RISXVERSION     "0.0.5"
+#define RISXCODENAME    "Initial commit"
+
+/* Maximum number of entries in the process table for user-mode processes     */
+#define PT_SIZE         32
+
+#endif
