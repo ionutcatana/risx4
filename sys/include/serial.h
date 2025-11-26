8cf3e12371f249dbd1493561189a9dba35b88271
Ioan-Alexandru Catana <ionutcatana@member.fsf.org>
2025-11-26 19:25:35 +0200

    remove useless puts func

--- sys/include/serial.h
+++ sys/include/serial.h
@@ -1,9 +1,8 @@
-#ifndef X86_SERIAL_H
-#define X86_SERIAL_H 1
+#ifndef SERIAL_H
+#define SERIAL_H 1
 
 // arch/x86/serial.c
 int initserial(void);
 void serialputchar(int c);
-void serialputs(const char* str);
 
 #endif
