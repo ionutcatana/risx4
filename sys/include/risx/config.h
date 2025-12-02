69bd877dc49ceaaca8d452af0dc0b9799d44476c
Ioan-Alexandru Catana <ionutcatana@member.fsf.org>
2025-12-02 16:34:45 +0200

    add somem more config stuff

--- sys/include/risx/config.h
+++ sys/include/risx/config.h
@@ -1,11 +1,16 @@
 #ifndef CONFIG_H
 #define CONFIG_H
 
-/* RISX release number and codename                                           */
+/* risx release number and codename                                           */
 #define RISXVERSION     "0.0.5"
 #define RISXCODENAME    "Initial commit"
 
-/* Maximum number of entries in the process table for user-mode processes     */
-#define PT_SIZE         32
+/* maximum number of entries in the process table for user-mode processes     */
+#define PT_SIZE         128
+
+/* physical page frame implementation                                         */
+#define PMM_BITMAP
+#define PMM_CURRENT     (PMM_BITMAP)
+#define PMM_DEFAULT     (PMM_BITMAP)
 
 #endif
