4629fd69dcf1b4e663196b28fb81821ef4ea86b1
Ioan-Alexandru Catana <ionutcatana@member.fsf.org>
2025-12-22 21:22:11 +0200

    move stack base to a safe location

--- sys/include/commonarch/paging.h
+++ sys/include/commonarch/paging.h
@@ -7,7 +7,7 @@
 #define PAGE_SIZE_2M        (0x200000)
 #define PAGE_SIZE_1G        (0x40000000)
 
-#define STACK_BASE_VIRT     ((uintptr_t)0xffffffff80000000)
+#define STACK_BASE_VIRT     ((uintptr_t)0xffffffffffe00000)
 
 #define LVL4_INDEX(va)      (((uintptr_t)(va) >> 39) & 0x1FF)
 #define LVL3_INDEX(va)      (((uintptr_t)(va) >> 30) & 0x1FF)
