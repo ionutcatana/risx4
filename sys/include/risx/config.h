25e511d43d2da82d7488e58f285a29ae0e8e2b94
Ioan-Alexandru Catana <ionutcatana@member.fsf.org>
2026-02-05 14:28:37 +0200

    remove some useless configs

--- sys/include/risx/config.h
+++ sys/include/risx/config.h
@@ -2,22 +2,15 @@
 #define CONFIG_H
 
 /* risx release number and codename                                           */
-#define RISX_VERSION    "0.0.6"
-#define RISX_CODENAME   "Initial commit"
+#define VERSION         "0.0.6"
+#define CODENAME        "Initial commit"
 
 /* maximum number of entries in the process table for user-mode processes     */
-#define PT_SIZE         128
-
-/* physical page frame implementation                                         */
-#define PMM_BITMAP
-#define PMM_STACK
-#define PMM_BUDDY
-#define PMM_CURRENT     (PMM_BITMAP)
-#define PMM_DEFAULT     (PMM_BITMAP)
+#define NPROCESS        128
 
 /* stack size and max core count options. all stacks fit on a single 2mib mega
    page. possible configs: 32 cores+64k stacks or 128 cores+16k stacks        */
 #define STACK_SIZE      65536
-#define MAX_CPU_COUNT   32
+#define NCPU            32
 
 #endif
