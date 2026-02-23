5aa711430ec649f4de97e47a2159a1aecc7133bd
Ioan-Alexandru Catana <ionutcatana@member.fsf.org>
2026-02-23 22:51:09 +0200

    wip 11

--- sys/include/arch/x86_64/specific/gdt.h
+++ sys/include/arch/x86_64/specific/gdt.h
@@ -3,18 +3,16 @@
 
 #include <stdint.h>
 
-#define GDT_SIZE        (7)
-
 #define RISX_CODE_SEG   ((uint16_t)0x08)
 #define RISX_DATA_SEG   ((uint16_t)0x10)
 #define USER_DATA_SEG   ((uint16_t)0x1b)
 #define USER_CODE_SEG   ((uint16_t)0x23)
-#define TSS_SEG         ((uint16_t)0x28)
+#define RISX_TSS0_SEG   ((uint16_t)0x28)
 
 typedef struct {
     uint16_t limit;
     uint64_t base;
-} __attribute__((packed)) gdtr_t;
+} __attribute__((packed)) gdtdesc_t;
 
 typedef union {
     struct {
@@ -26,7 +24,7 @@ typedef union {
         uint8_t base_upper;
     } __attribute__((packed));
     uint64_t value;
-} gdtsegdesc_t;
+} segdesc_t;
 
 typedef struct {
     uint32_t reserved0;
@@ -42,7 +40,7 @@ typedef struct {
 
 
 // arch/x86/gdt.S
-void loadgdt(gdtr_t* desc);
+void loadgdt(void);
 void loadtr(uint16_t selector);
 
 // arch/x86/gdt.c
