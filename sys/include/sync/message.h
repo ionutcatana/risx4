98338b9ecb041c8ce78e0cb4ce83d2cf78a73be0
Ioan-Alexandru Catana <ionutcatana@member.fsf.org>
2026-03-04 16:52:07 +0200

    change message_t type

--- sys/include/sync/message.h
+++ sys/include/sync/message.h
@@ -3,9 +3,15 @@
 
 #include <stdint.h>
 
-typedef uint64_t message_t;
+typedef struct message_t {
+    uint32_t sender;
+    uint32_t type;
+    union {
+        uint64_t part[3];   // work in progress
+    } payload;
+} __attribute__((packed)) message_t;
 
-int send(int dest, message_t msg);
+int send(int dest, message_t* msg);
 int receive(int src, message_t* msg);
 
 #endif
