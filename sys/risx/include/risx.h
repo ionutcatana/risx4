2cea8d39ba3d1ddef69fa73b1b8a3955d0953f37
Ioan-Alexandru Catana <ionutcatana@member.fsf.org>
2026-03-09 22:55:20 +0200

    add useful pair struct

--- sys/risx/include/risx.h
+++ sys/risx/include/risx.h
@@ -5,6 +5,11 @@
 #include <stdint.h>
 #include <stdnoreturn.h>
 
+typedef struct {
+    uint64_t value0; // stack top address
+    uint64_t value1; // physical address of kernel page table
+} pair_t;
+
 // risx.c
 void setup();
 noreturn void risx(void);
