dfae2c4d097acdef549f798370dde984a3599fb8
Ioan-Alexandru Catana <ionutcatana@member.fsf.org>
2026-04-20 23:56:38 +0300

    refactor: move contents of proc.h to process.h

--- sys/risx/include/process.h
+++ sys/risx/include/process.h
@@ -1,6 +1,44 @@
 #ifndef PROCESS_H
 #define PROCESS_H 1
 
+#include "commonarch/interrupts.h"
+#include "commonarch/paging.h"
+#include "risx/config.h"
+
+#include <stddef.h>
+#include <stdint.h>
+
+enum procstate {
+	UNUSED,
+	EMBRYO,
+	SLEEPING,
+	RUNNABLE,
+	RUNNING,
+	ZOMBIE
+};
+
+struct cpu {
+	uint32_t lapicid;
+};
+
+struct proc {
+	uint32_t pid;
+	uint32_t uid;
+	uint32_t gid;
+	uint64_t entry;
+	uint64_t stack;
+	enum procstate state;
+	struct pagetable* pagetable;
+	struct trapframe* trapframe;
+	struct proc* parent;
+	struct cpu* cpu;
+};
+
+struct cpu* cpus(void);
+struct proc* proctable(void);
+size_t proccount(void);
+
+void initprocesses(void);
 void schedule(void);
 
 #endif
