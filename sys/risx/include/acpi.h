60b350568108e0e229ecc39a8809bc69ff4868fb
Ioan-Alexandru Catana <ionutcatana@member.fsf.org>
2026-04-20 18:24:59 +0300

    remove commented clutter

--- sys/risx/include/acpi.h
+++ sys/risx/include/acpi.h
@@ -5,23 +5,4 @@
 #include "acpi/defs.h"
 #include "acpi/types.h"
 
-// struct rsdt {
-//     struct sdtheader header;
-//     uint32_t sdtaddr[(header.length - sizeof(struct sdtheader)) / 4];
-// } __attribute__((packed));
-// struct xsdt {
-//     struct sdtheader header;
-//     uint64_t sdtaddr[(header.length - sizeof(struct sdtheader)) / 8];
-// } __attribute__((packed));
-// struct reserved_madt_record {
-// } __attribute__((packed));
-// #define MADT_RECORD_FLAG_POLARITY_NO_OVERRIDE (0)
-// #define MADT_RECORD_FLAG_POLARITY_DEFAULT (0)
-// #define MADT_RECORD_FLAG_POLARITY_ACTIVE_HIGH_OVERRIDE (1)
-// #define MADT_RECORD_FLAG_POLARITY_ACTIVE_LOW_OVERRIDE (3)
-// #define MADT_RECORD_FLAG_TRIGGER_MODE_NO_OVERRIDE (0)
-// #define MADT_RECORD_FLAG_TRIGGER_MODE_DEFAULT (0)
-// #define MADT_RECORD_FLAG_TRIGGER_MODE_EDGE_TRIGGERED (1)
-// #define MADT_RECORD_FLAG_TRIGGER_MODE_LEVEL_TRIGGERED (3)
-
 #endif
